de024fcbc120665deb4efd6af5e45d1d9d79e72c
Ajaypal Ghuman <asingh0328@yahoo.com>
2022-06-09 20:01:09 -0400

    updated server.js

--- server/server.js
+++ server/server.js
@@ -5,8 +5,8 @@ const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
 const { authMiddleware } = require("./utils/auth");
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
 const startServer = async () => {
   const server = new ApolloServer({
@@ -18,16 +18,21 @@ const startServer = async () => {
   server.applyMiddleware({ app });
   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 };
+
 startServer();
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build"));
+  });
 }
-
 db.once("open", () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
 });
